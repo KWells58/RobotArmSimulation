496a51992b2eae426e26a57cba3192e7e0fff84c
Kelly <kelly.wells10000@gmail.com>
2025-12-05 14:41:48 -0500

    undo

--- src/simulation/robo_env.py
+++ src/simulation/robo_env.py
@@ -1,7 +1,3 @@
-import warnings
-warnings.filterwarnings("ignore", category=UserWarning, module="robosuite")
-
-
 import robosuite
 from robosuite.controllers import load_part_controller_config as load_controller_config
 import numpy as np
